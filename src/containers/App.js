f1f2ba8700018ac2b702097fffb426fdf83667ea
vincent <vincent.arteaga@gmail.com>
2021-07-12 22:31:39 -0700

    assignment complete

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import Home from '../components/Home';
-import Actors from '../components/Actors';
-import Directors from '../components/Directors';
-import Movies from '../components/Movies';
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import Home from "../components/Home";
+import Actors from "../components/Actors";
+import Directors from "../components/Directors";
+import Movies from "../components/Movies";
 
 const App = (props) => {
   return (
     <Router>
       {/*{code here}*/}
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/actors" component={Actors} />
+      </div>
     </Router>
   );
 };
 
-export default App
+export default App;
